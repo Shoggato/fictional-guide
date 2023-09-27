affef41067a9ce16c39a28866c26e234b50d3e88
Shoggato <eeels1986@gmail.com>
2023-09-26 19:51:21 -0500

    finished the HTML part of this project

---
+++ 14.3 -JavaScript with D3.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>14.3 -JavaScript with D3.js</title>
+</head>
+<body>
+    <h1>14.3 -JavaScript with D3.js</h1>
+    <p><strong>Overview</strong></p><br>
+    <p>In this lesson, you will learn how to add custom interactivity to your web visualizations. 
+        First, you’ll be introduced to basic document object model (DOM) selection, manipulation, and events using D3.jsLinks to an external site., an industry-standard data visualization library written in JavaScript. 
+        The lesson will begin with an overview of D3 and D3.json from your instructor. Then, the lesson will turn to D3 Select and append. 
+        After a brief overview from your instructor, you will complete an activity using D3 to add a new row of data to a table. 
+        Then, after a walkthrough and activity using the this keyword, class will finish with a demonstration and activity on how to create dropdown menus by using Plotly.</p><br>
+    <p><strong>What You'll Learn</strong></p><br>
+    <p>By the end of this lesson, you will be able to:</p><br>
+    <ul>
+        <li>Create charts by using data from API calls.</li>
+        <li>Use D3 for basic document object model (DOM) manipulation and event handling.</li>
+        <li>Apply the this keyword to reference elements within a function.</li>
+        <li>Dynamically manipulate the DOM through events.</li>
+        <li>Manipulate charts through dropdown events and click events.</li>
+        <li>Use Plotly.restyle() to create dynamic charts.</li>
+    </ul>
+    <p>Table of Contents:</p>
+    <a href="Part1 ">Go to Part1</a><br>
+    <a href="Part2 - Execute Python in non-Terminal programs">Go to Part2</a><br>
+    <a href="Part3 - Execute Python in self-contained IDEs">Go to Part3</a><br>
+    <a href="Part4 - Set up Jupyter notebook (IMPORTANT)">Go to Part4</a><br>
+    <a href="Part5 - Execute Python Code Online (IMPORTANT)">Go to Part5</a><br>
+</body>
+</html>
